f265504c0829dfca91f7c1e399b40894bd44223b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,69 +1,65 @@
-output "monitor_metric_alerts" {
-  description = "All monitor_metric_alert resources"
-  value       = azurerm_monitor_metric_alert.monitor_metric_alerts
-}
 output "monitor_metric_alerts_action" {
-  description = "List of action values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.action]
+  description = "Map of action values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.action }
 }
 output "monitor_metric_alerts_application_insights_web_test_location_availability_criteria" {
-  description = "List of application_insights_web_test_location_availability_criteria values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.application_insights_web_test_location_availability_criteria]
+  description = "Map of application_insights_web_test_location_availability_criteria values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.application_insights_web_test_location_availability_criteria }
 }
 output "monitor_metric_alerts_auto_mitigate" {
-  description = "List of auto_mitigate values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.auto_mitigate]
+  description = "Map of auto_mitigate values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.auto_mitigate }
 }
 output "monitor_metric_alerts_criteria" {
-  description = "List of criteria values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.criteria]
+  description = "Map of criteria values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.criteria }
 }
 output "monitor_metric_alerts_description" {
-  description = "List of description values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.description]
+  description = "Map of description values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.description }
 }
 output "monitor_metric_alerts_dynamic_criteria" {
-  description = "List of dynamic_criteria values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.dynamic_criteria]
+  description = "Map of dynamic_criteria values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.dynamic_criteria }
 }
 output "monitor_metric_alerts_enabled" {
-  description = "List of enabled values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.enabled]
+  description = "Map of enabled values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.enabled }
 }
 output "monitor_metric_alerts_frequency" {
-  description = "List of frequency values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.frequency]
+  description = "Map of frequency values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.frequency }
 }
 output "monitor_metric_alerts_name" {
-  description = "List of name values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.name]
+  description = "Map of name values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.name }
 }
 output "monitor_metric_alerts_resource_group_name" {
-  description = "List of resource_group_name values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.resource_group_name]
+  description = "Map of resource_group_name values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.resource_group_name }
 }
 output "monitor_metric_alerts_scopes" {
-  description = "List of scopes values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.scopes]
+  description = "Map of scopes values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.scopes }
 }
 output "monitor_metric_alerts_severity" {
-  description = "List of severity values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.severity]
+  description = "Map of severity values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.severity }
 }
 output "monitor_metric_alerts_tags" {
-  description = "List of tags values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.tags]
+  description = "Map of tags values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.tags }
 }
 output "monitor_metric_alerts_target_resource_location" {
-  description = "List of target_resource_location values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.target_resource_location]
+  description = "Map of target_resource_location values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.target_resource_location }
 }
 output "monitor_metric_alerts_target_resource_type" {
-  description = "List of target_resource_type values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.target_resource_type]
+  description = "Map of target_resource_type values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.target_resource_type }
 }
 output "monitor_metric_alerts_window_size" {
-  description = "List of window_size values across all monitor_metric_alerts"
-  value       = [for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : v.window_size]
+  description = "Map of window_size values across all monitor_metric_alerts, keyed the same as var.monitor_metric_alerts"
+  value       = { for k, v in azurerm_monitor_metric_alert.monitor_metric_alerts : k => v.window_size }
 }
 
